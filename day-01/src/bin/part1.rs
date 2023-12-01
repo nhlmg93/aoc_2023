538c45c98ce0fad833f609ad7dfccf6d9d430766
Nathan Helmig <nathan.helmig@itility.com>
2023-12-01 08:31:39 -0600

    day 1

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,20 +1,41 @@
-fn main() {
+use std::io;
+
+#[derive(Debug)]
+struct AocError(String);
+
+fn main() -> Result<(), io::Error> {
     let input = include_str!("input1.txt");
     let output = part1(input);
-    dbg!(output);
+    dbg!(output).ok();
+    Ok(())
 }
-
-fn part1(input: &str) -> String {
-    print!("{}", input);
-    input.to_string()
+fn part1(input: &str) -> Result<u32, AocError> {
+    Ok(input
+        .lines()
+        .map(|line| {
+            let mut it = line.chars().filter_map(|character| character.to_digit(10));
+            let first = it.next().expect("should be number");
+            match it.last() {
+                Some(num) => format!("{first}{num}").parse::<u32>(),
+                None => format!("{first}{first}").parse::<u32>(),
+            }
+            .expect("should be valid num")
+        })
+        .sum::<u32>())
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
     #[test]
-    fn it_works() {
-        let result = part1("4");
-        assert_eq!(result, "4".to_string())
+    fn it_works() -> Result<(), AocError> {
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+";
+        let result = part1(input)?;
+        assert_eq!(result, 142);
+        Ok(())
     }
 }
--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -1,20 +1,68 @@
-fn main() {
-    let input = include_str!("input1.txt");
-    let output = part2(input);
-    dbg!(output);
-}
+use std::io;
 
-fn part2(input: &str) -> String {
-    print!("{}", input);
-    input.to_string()
+#[derive(Debug)]
+struct AocError(String);
+
+fn main() -> Result<(), io::Error> {
+    let input = include_str!("input2.txt");
+    let output = part2(input);
+    dbg!(output).ok();
+    Ok(())
+}
+fn part2(input: &str) -> Result<u32, AocError> {
+    Ok(input
+        .lines()
+        .map(|line| {
+            let mut it = (0..line.len()).filter_map(|index| {
+                let reduced_line = &line[index..];
+                let result = if reduced_line.starts_with("one") {
+                    '1'
+                } else if reduced_line.starts_with("two") {
+                    '2'
+                } else if reduced_line.starts_with("three") {
+                    '3'
+                } else if reduced_line.starts_with("four") {
+                    '4'
+                } else if reduced_line.starts_with("five") {
+                    '5'
+                } else if reduced_line.starts_with("six") {
+                    '6'
+                } else if reduced_line.starts_with("seven") {
+                    '7'
+                } else if reduced_line.starts_with("eight") {
+                    '8'
+                } else if reduced_line.starts_with("nine") {
+                    '9'
+                } else {
+                    reduced_line.chars().next().unwrap()
+                };
+
+                result.to_digit(10)
+            });
+            let first = it.next().expect("should be number");
+            match it.last() {
+                Some(num) => format!("{first}{num}").parse::<u32>(),
+                None => format!("{first}{first}").parse::<u32>(),
+            }
+            .expect("should be valid num")
+        })
+        .sum::<u32>())
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
     #[test]
-    fn it_works() {
-        let result = part2("4");
-        assert_eq!(result, "4".to_string())
+    fn it_works() -> Result<(), AocError> {
+        let input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+        let result = part2(input)?;
+        assert_eq!(result, 281);
+        Ok(())
     }
 }
